44d85a35973e72c4667190def7c0842268d45192
Paige <paigelowee@aol.com>
2022-12-09 21:06:34 +0000

    attempt day7

--- day7/day7.py
+++ day7/day7.py
@@ -2,42 +2,50 @@ def main():
   def soln1():
     with open('day7/input.txt', 'r') as file:
       # maintain current path
-      path = ['/']
+      file_struct = {}
       file_sizes = {}
+      curr_file = '/'
       for line in file:
         line = line.replace('\n', '')
 
         # handle commands (directory navigation)
         if line.startswith('$'):
-          print(line)
           if ' cd ' in  line:
-            directory = line.strip('$ cd')
-            print(directory)
-            if directory == '/':
-              path = ['/']
-            elif directory == '..':
-              path.pop()
-            else:
-              path.append(directory)
-        print(path)
+            directory = line.replace('$ cd', '').replace(' ', '')
+            curr_file = directory
 
         # handle directory content
-        # else:
-        #   if not line.startswith('dir'):
-        #     size = int(line.split(' ')[0])
-        #     current_path = ''
-        #     # print(path)
-        #     for directory in path:
-        #       print(directory)
-        #       print(current_path)
-        #       current_path += directory + '/' if directory != '/' else '/'
-        #       file_sizes[current_path] = file_sizes.get('current_path', 0) + size
+        else:
+          # add to curr dir size
+          if curr_file not in file_sizes:
+            file_sizes[curr_file] = 0
+          if curr_file not in file_struct:
+            file_struct[curr_file] = []
+          if not line.startswith('dir'):
+            size = int(line.split(' ')[0])
+            file_sizes[curr_file] += size
+          else:
+            dir = line.split(' ')[1]
+            file_sizes
+            file_struct[curr_file].append(dir)
+      total_size = 0
 
-      print(file_sizes)
+      for k, v in file_struct.items():
+        dir_size = get_file_size(k, file_sizes, file_struct)
 
+        if dir_size <= 100000:
+          total_size += dir_size
 
-      print(path)
+      print(total_size)
+
+  def get_file_size(key, file_sizes, file_struct):
+    size = file_sizes[key]
+    for dir in file_struct[key]:
+      size += get_file_size(dir, file_sizes, file_struct)
+    return size
+
   soln1()
+
 
 if __name__ == '__main__':
   main()
